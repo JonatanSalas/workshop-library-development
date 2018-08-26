77ea7a7c0d4aa3603da9c1ed153ad8877f6aaacd
jonatansalas <jonatansalas@live.com.ar>
2018-08-26 18:43:06 -0300

    Initial commit

---
+++ theme.js
@@ -0,0 +1,13 @@
+import { yellow as theme } from 'mdx-deck/themes';
+
+export const custom = {
+    ...theme,
+    font: 'Ubuntu',
+    colors: {
+        text: '#FFFFFF',
+        link: '#E91E63',
+        background: '#673AB7',
+        codeBackground: '#311B92',
+        preBackground: '#311B92'
+    }
+}
